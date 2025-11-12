268946e93a939a3276f5ca5098294c13fb94bb51
Maria Elena Ayala Martinez <mariaelenaayala10@outlook.com>
2025-11-12 11:35:35 -0600

    modifique examenDTO, AgrgarExamen, EditarExamen y Examenes Guardar no funciona la fecha de cita y estado y editar si la le cancelo el estado se quita de la listado de examenes el registro

--- LaboratorioClinicoApp/DTO/ExamenDTO.cs
+++ LaboratorioClinicoApp/DTO/ExamenDTO.cs
@@ -1,17 +1,28 @@
-﻿namespace LaboratorioClinicoApp.DTO
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LaboratorioClinicoApp.DTO
 {
     public class ExamenDTO
-
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "⚠️ El tipo de examen es requerido.")]
         public string TipoExamen { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "⚠️ La fecha es requerida.")]
         public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "⚠️ La descripción es requerida.")]
         public string Descripcion { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "⚠️ El estado es requerido.")]
         public bool Estado { get; set; }
+
+        [Required(ErrorMessage = "⚠️ El ID de la cita es requerido.")]
         public int IdCita { get; set; }
+
+        [Required(ErrorMessage = "⚠️ El ID del paciente es requerido.")]
         public int IdPaciente { get; set; }
-
     }
-
 }
-
